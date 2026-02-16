5f27a7284c689c59c9075f6ce4bb75f2eda0edfc
Forinite <obefortune2@gmail.com>
2026-02-16 16:45:42 +0100

    Search Engine Optimization Update_1

--- find-mx-dine/src/App.jsx
+++ find-mx-dine/src/App.jsx
@@ -1,11 +1,19 @@
 // src/App.jsx:1
 
 import MapComponent from "./components/MapComponent";
+import SpecificMap from "./components/SpecificMap.jsx";
+import {restaurants} from "./data/restaurants.js";
 
 function App() {
     return (
         <div style={{ height: "100vh", width: "100%", overflow: "hidden" }}>
-            <MapComponent />
+            <div style={{ height: "300px", width: "300px", overflow: "hidden" }}>
+                <MapComponent />
+            </div>
+            <p>Specific</p>
+            <div style={{ height: "300px", width: "300px", overflow: "hidden" }}>
+                <SpecificMap restaurant={restaurants[0]} />
+            </div>
         </div>
     )
 }
